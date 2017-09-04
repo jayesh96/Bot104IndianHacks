adadb3d8c290549374a5450ca0f648dd119a97b8
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-09-04 13:35:41 +0530

    Added static url

--- app/src/helloworld/helloworld/settings.py
+++ app/src/helloworld/helloworld/settings.py
@@ -73,24 +73,24 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'hasuradb',
-#          'USER': 'admin',
-#          'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#          'HOST': os.environ.get('POSTGRES_HOST'),
-#          'PORT': '5432',
-#      }
-#  }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'hasuradb',
+         'USER': 'admin',
+         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+         'HOST': os.environ.get('POSTGRES_HOST'),
+         'PORT': '5432',
+     }
+ }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
--- app/src/helloworld/helloworld/urls.py
+++ app/src/helloworld/helloworld/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 from helloworld import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
