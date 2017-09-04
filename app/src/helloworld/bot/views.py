4d28b78aa3d3cbf85fbf0335815a86c197fbc341
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-09-04 17:13:44 +0530

    Views Hlist

--- app/src/helloworld/bot/views.py
+++ app/src/helloworld/bot/views.py
@@ -8,6 +8,7 @@ def HospitalList(request):
 	h_list = []
 	obj = Hospital.objects.all()
 	for i in obj:
+
 		a = {}
 		a['hospital_name'] = i.title
 		a['address'] = i.address
@@ -16,7 +17,7 @@ def HospitalList(request):
 		a['avg_rating'] = i.avg_rating
 		h_list.append(a)
 
-	print h_list
+	print (h_list)
 
 
 	return JsonResponse(h_list,safe=False,json_dumps_params={'indent': 2})
