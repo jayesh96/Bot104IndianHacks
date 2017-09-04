35f8e81cd65e51104e57ea8d0d7e845e6c78a72e
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-09-04 15:17:46 +0530

    Minor

--- app/scripts/migrate.sh
+++ app/scripts/migrate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # First, makemigrations and migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(email='test@test.com').delete(); User.objects.create_superuser('test', 'test@test.com', 'pass1234')" | python manage.py shell
+python3 manage.py collectstatic
 python3 manage.py makemigrations
 python3 manage.py migrate
 
