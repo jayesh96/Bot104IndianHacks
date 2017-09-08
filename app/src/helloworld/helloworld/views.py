a265a555996d7e94beec11f5f570cfe00d9336fd
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-09-08 17:49:06 +0530

    New Models

--- app/src/helloworld/helloworld/views.py
+++ app/src/helloworld/helloworld/views.py
@@ -12,7 +12,6 @@ def index(request):
 
 	hospital = Hospital.objects.get(title = request.GET.get('hospital'))
 	obj = HospitalBeds.objects.get(title_id=hospital.id)
-	print (obj)
 	context = {
 			"hospital":hospital,
 			"hospital_details":obj
@@ -20,26 +19,21 @@ def index(request):
 		}
 
 	if request.method=="POST":
-		print "-----------------"
-		print request.POST
+		
 		hospital_name = request.POST.get('hospital_name')
 		bed_type = request.POST.get("bed")
-		print (hospital_name,bed_type)
 		hospital = Hospital.objects.get(title = hospital_name)
 		bed_book = HospitalBooking()
 		bed_book.title_id = hospital.id
 		bed_book.bed_type = bed_type
 		bed_book.save()
-		print bed_book
 
 		obj_bed = HospitalBeds.objects.get(title_id=hospital.id)
 		if bed_book.bed_type == 'A':
 			obj_bed.room_type_a = obj_bed.room_type_a - 1
 			obj_bed.save()
-			print obj_bed,bed_book.bed_type
 		else:
 			obj_bed.room_type_b = obj_bed.room_type_b - 1
 			obj_bed.save()
-			print obj_bed,bed_book.bed_type
 	return render(request,"book-a-bed.html",context)
 
